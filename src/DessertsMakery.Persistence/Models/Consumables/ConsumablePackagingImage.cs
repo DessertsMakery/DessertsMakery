c6832ad911af77bb539ed7c8e9923dc9d4d34e3a
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2024-02-28 11:34:37 +0200

    feat: add image for consumable packaging (#3)

---
+++ src/DessertsMakery.Persistence/Models/Consumables/ConsumablePackagingImage.cs
@@ -0,0 +1,9 @@
+﻿namespace DessertsMakery.Persistence.Models.Consumables;
+
+public sealed class ConsumablePackagingImage : Entity
+{
+    public byte[] Image { get; set; } = null!;
+
+    public long ConsumablePackagingId { get; set; }
+    public ConsumablePackaging ConsumablePackaging { get; set; } = null!;
+}
