9bbdc5d780eea3e59ab851e9bc910557999d9b7d
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2024-02-28 10:34:30 +0200

    feat: add entity

---
+++ src/DessertsMakery.Persistence/Models/Entity.cs
@@ -0,0 +1,10 @@
+﻿namespace DessertsMakery.Persistence.Models;
+
+public abstract class Entity
+{
+    public long InternalId { get; set; }
+    public Guid ExternalId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime ModifiedAt { get; set; }
+    public bool IsDeleted { get; set; }
+}
