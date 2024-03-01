929a59981f3e629fe0860bd2e73acd60057bee8b
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2024-03-01 15:31:07 +0200

    fix: remove redundant usings

--- src/DessertsMakery.Persistence/Models/Essentials/Addition.cs
+++ src/DessertsMakery.Persistence/Models/Essentials/Addition.cs
@@ -1,6 +1,3 @@
-﻿using DessertsMakery.Persistence.Models.Consumables;
-using DessertsMakery.Persistence.Models.Orders;
-
-namespace DessertsMakery.Persistence.Models.Essentials;
+﻿namespace DessertsMakery.Persistence.Models.Essentials;
 
 public sealed class Addition : Component { }
--- src/DessertsMakery.Persistence/Models/Essentials/Ingredient.cs
+++ src/DessertsMakery.Persistence/Models/Essentials/Ingredient.cs
@@ -1,6 +1,3 @@
-﻿using DessertsMakery.Persistence.Models.Consumables;
-using DessertsMakery.Persistence.Models.Recipes;
-
-namespace DessertsMakery.Persistence.Models.Essentials;
+﻿namespace DessertsMakery.Persistence.Models.Essentials;
 
 public sealed class Ingredient : Component { }
