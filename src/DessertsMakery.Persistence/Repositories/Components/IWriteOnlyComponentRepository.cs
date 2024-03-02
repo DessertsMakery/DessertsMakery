d8d9b0d3281981922c845e2d6a00261a49c4a58b
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2024-03-02 15:06:40 +0200

    feat: add repository for components

---
+++ src/DessertsMakery.Persistence/Repositories/Components/IWriteOnlyComponentRepository.cs
@@ -0,0 +1,18 @@
+﻿using DessertsMakery.Persistence.Models.Essentials;
+
+namespace DessertsMakery.Persistence.Repositories.Components;
+
+public interface IWriteOnlyComponentRepository
+{
+    ValueTask CreateAsync<TComponent>(TComponent component, CancellationToken token = default)
+        where TComponent : Component;
+
+    ValueTask UpdateAsync<TComponent>(
+        Guid externalId,
+        Action<TComponent> modification,
+        CancellationToken token = default
+    )
+        where TComponent : Component;
+
+    ValueTask DeleteAsync(Guid externalId, bool isHardDelete = false, CancellationToken token = default);
+}
---
+++ src/DessertsMakery.Persistence/Repositories/Components/ReadWriteComponentRepository.cs
@@ -0,0 +1,158 @@
+﻿using System.Linq.Expressions;
+using DessertsMakery.Persistence.Database.Interfaces;
+using DessertsMakery.Persistence.Models.Essentials;
+using Microsoft.EntityFrameworkCore;
+
+namespace DessertsMakery.Persistence.Repositories.Components;
+
+internal sealed class ReadWriteComponentRepository : IReadWriteComponentRepository, IRepository
+{
+    private readonly IEssentialTables _essentialTables;
+
+    public ReadWriteComponentRepository(IEssentialTables essentialTables)
+    {
+        _essentialTables = essentialTables;
+    }
+
+    #region Get With Filtering
+
+    public async Task<IReadOnlyCollection<Component>> GetComponentsAsync(
+        Expression<Func<Component, bool>>? filter = null,
+        CancellationToken token = default
+    ) => await GetComponentRowsAsync(_essentialTables.Components, filter, token);
+
+    public async Task<IReadOnlyCollection<Ingredient>> GetIngredientsAsync(
+        Expression<Func<Ingredient, bool>>? filter = null,
+        CancellationToken token = default
+    ) => await GetComponentRowsAsync(_essentialTables.Ingredients, filter, token);
+
+    public async Task<IReadOnlyCollection<Addition>> GetAdditionsAsync(
+        Expression<Func<Addition, bool>>? filter = null,
+        CancellationToken token = default
+    ) => await GetComponentRowsAsync(_essentialTables.Additions, filter, token);
+
+    public async Task<IReadOnlyCollection<PackagingComponent>> GetPackagingComponentsAsync(
+        Expression<Func<PackagingComponent, bool>>? filter = null,
+        CancellationToken token = default
+    ) => await GetComponentRowsAsync(_essentialTables.PackagingComponents, filter, token);
+
+    #endregion
+
+    #region Get By Id
+
+    public Task<Component?> GetComponentByIdAsync(Guid externalId, CancellationToken token = default) =>
+        GetComponentByIdAsync(_essentialTables.Components, externalId, token);
+
+    public Task<Ingredient?> GetIngredientByIdAsync(Guid externalId, CancellationToken token = default) =>
+        GetComponentByIdAsync(_essentialTables.Ingredients, externalId, token);
+
+    public Task<Addition?> GetAdditionByIdAsync(Guid externalId, CancellationToken token = default) =>
+        GetComponentByIdAsync(_essentialTables.Additions, externalId, token);
+
+    public Task<PackagingComponent?> GetPackagingComponentByIdAsync(
+        Guid externalId,
+        CancellationToken token = default
+    ) => GetComponentByIdAsync(_essentialTables.PackagingComponents, externalId, token);
+
+    #endregion
+
+    #region Create
+
+    public async ValueTask CreateAsync<TComponent>(TComponent component, CancellationToken token = default)
+        where TComponent : Component
+    {
+        await _essentialTables.Components.AddAsync(component, token);
+    }
+
+    #endregion
+
+    #region Update
+
+    public async ValueTask UpdateAsync<TComponent>(
+        Guid externalId,
+        Action<TComponent> modification,
+        CancellationToken token = default
+    )
+        where TComponent : Component
+    {
+        var table = _essentialTables.Components;
+        var component = await GetComponentByIdAsync(table, externalId, token);
+        var concrete = ConvertOrThrowIfNotCorrectComponentType<TComponent>(externalId, component);
+
+        modification(concrete);
+        table.Update(concrete);
+    }
+
+    #endregion
+
+    #region Delete
+
+    public async ValueTask DeleteAsync(Guid externalId, bool isHardDelete = false, CancellationToken token = default)
+    {
+        if (isHardDelete)
+        {
+            var table = _essentialTables.Components;
+            var component = await GetComponentByIdAsync(table, externalId, token);
+            ThrowIfComponentIsNull(externalId, component);
+            _essentialTables.Components.Remove(component!);
+            return;
+        }
+
+        await UpdateAsync<Component>(externalId, component => component.IsDeleted = true, token);
+    }
+
+    #endregion
+
+    #region Helpers
+
+    private static Task<TComponent[]> GetComponentRowsAsync<TComponent>(
+        DbSet<TComponent> table,
+        Expression<Func<TComponent, bool>>? filter,
+        CancellationToken token
+    )
+        where TComponent : Component
+    {
+        var queryable = table.AsQueryable();
+        if (filter is not null)
+        {
+            queryable = queryable.Where(filter);
+        }
+
+        return queryable.ToArrayAsync(token);
+    }
+
+    private static Task<TComponent?> GetComponentByIdAsync<TComponent>(
+        DbSet<TComponent> table,
+        Guid externalId,
+        CancellationToken token
+    )
+        where TComponent : Component
+    {
+        return table.AsQueryable().FirstOrDefaultAsync(x => x.ExternalId == externalId, token);
+    }
+
+    private static TComponent ConvertOrThrowIfNotCorrectComponentType<TComponent>(Guid externalId, Component? component)
+        where TComponent : Component
+    {
+        ThrowIfComponentIsNull(externalId, component);
+
+        if (component is not TComponent concrete)
+        {
+            throw new InvalidOperationException(
+                $"Requested component by external id: `{externalId}` is not of type `{typeof(TComponent)}`"
+            );
+        }
+
+        return concrete;
+    }
+
+    private static void ThrowIfComponentIsNull(Guid externalId, Component? component)
+    {
+        if (component is null)
+        {
+            throw new InvalidOperationException($"Requested component by external id: `{externalId}` is not found");
+        }
+    }
+
+    #endregion
+}
